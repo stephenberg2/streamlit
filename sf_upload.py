134c82aa33a90aa461473a833841d044082c4df3
Stephen Berg <109757917+stephenberg2@users.noreply.github.com>
2023-09-22 11:22:05 -0400

    Update sf_upload.py

--- sf_upload.py
+++ sf_upload.py
@@ -1,8 +1,3 @@
-from snowflake.snowpark.session import Session
-from snowflake.snowpark import functions as F
-from snowflake.snowpark.types import *
-from snowflake.snowpark import Window
-
 from io import StringIO
 import streamlit as st
 import pandas as pd
