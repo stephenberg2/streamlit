57bd399eb657d79a54d4c88258e6e8b4c26a3927
Stephen Berg <109757917+stephenberg2@users.noreply.github.com>
2023-09-22 11:19:16 -0400

    Create sf_upload.py

---
+++ sf_upload.py
@@ -0,0 +1,10 @@
+from snowflake.snowpark.session import Session
+from snowflake.snowpark import functions as F
+from snowflake.snowpark.types import *
+from snowflake.snowpark import Window
+
+from io import StringIO
+import streamlit as st
+import pandas as pd
+import json
+import numpy as np
